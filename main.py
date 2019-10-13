50df99a439d6d295d6833d1c2306749ce1fa3bb6
Kyle Thomas <mr.kyle.thomas@gmail.com>
2019-10-12 22:43:03 -0700

    added language stuff into main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template
 import urllib.request
 import requests as rq
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 
 app = Flask(__name__)
@@ -12,8 +15,10 @@ def handle_data():
     url = request.form['url']
 
     with urllib.request.urlopen(url) as fp:
-        soup = BeautifulSoup(fp, "html.parser")
-    text = soup.get_text()
+        html = fp.read()
+#        soup = BeautifulSoup(fp, "html.parser")
+#    text = soup.get_text()
+    get_sentiment(html)
 
     return render_template('show_results.html')
 
@@ -21,3 +26,21 @@ def handle_data():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+def get_sentiment(html):
+    # Instantiates a client
+    client = language.LanguageServiceClient()
+    
+    # The text to analyze
+    #text = u'Hello, world!'
+    document = types.Document(
+        content=html,
+        type=enums.Document.Type.HTML)
+    
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    
+    #print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
