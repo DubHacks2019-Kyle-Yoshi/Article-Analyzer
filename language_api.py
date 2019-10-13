fd44ac437f93c928a39a30ac22d3ba397a3e2949
Kyle Thomas <mr.kyle.thomas@gmail.com>
2019-10-12 22:37:58 -0700

    fixed having types instead of type

--- language_api.py
+++ language_api.py
@@ -10,7 +10,7 @@ client = language.LanguageServiceClient()
 text = u'Hello, world!'
 document = types.Document(
     content=text,
-    types=enums.Document.Type.PLAIN_TEXT)
+    type=enums.Document.Type.PLAIN_TEXT)
 
 # Detects the sentiment of the text
 sentiment = client.analyze_sentiment(document=document).document_sentiment
